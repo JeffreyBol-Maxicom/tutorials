efe328982234bfdc0b4d493a51c0a83d0bf0f2ae
JeffreyBol-Maxicom <jeffrey.bol@maxicom-it.com>
2025-05-12 17:09:46 +0200

    chapter 1 - 3

---
+++ estate/models/estate_property.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+class Property(models.Model):
+	_name = "estate.property"
+	_description = "Real estate properties"
+
+	name = fields.Char(
+		required = True
+	)
+	description = fields.Text()
+	postcode = fields.Char()
+	date_availability = fields.Date()
+	expected_price = fields.Float(
+		required = True
+	)
+	selling_price = fields.Float()
+	bedrooms = fields.Integer()
+	living_erea = fields.Integer()
+	facades = fields.Integer()
+	garage = fields.Boolean()
+	garden = fields.Boolean()
+	garden_erea = fields.Integer()
+	garden_orientation = fields.Selection(
+		selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+	)
