441c6daa9525e92ec3c2072e74f53cf2caa98ca2
JeffreyBol-Maxicom <jeffrey.bol@maxicom-it.com>
2025-06-30 16:08:41 +0200

    code reformat

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,43 @@
 from odoo import fields, models
 
-class Property(models.Model):
-	_name = "estate.property"
-	_description = "Real estate properties"
 
-	name = fields.Char(
-		required = True
-	)
-	description = fields.Text()
-	postcode = fields.Char()
-	date_availability = fields.Date()
-	expected_price = fields.Float(
-		required = True
-	)
-	selling_price = fields.Float()
-	bedrooms = fields.Integer()
-	living_erea = fields.Integer()
-	facades = fields.Integer()
-	garage = fields.Boolean()
-	garden = fields.Boolean()
-	garden_erea = fields.Integer()
-	garden_orientation = fields.Selection(
-		selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
-	)
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate properties"
+
+    name = fields.Char(
+        required=True
+    )
+
+    description = fields.Text()
+
+    postcode = fields.Char()
+
+    date_availability = fields.Date()
+
+    expected_price = fields.Float(
+        required=True
+    )
+
+    selling_price = fields.Float()
+
+    bedrooms = fields.Integer()
+
+    living_erea = fields.Integer()
+
+    facades = fields.Integer()
+
+    garage = fields.Boolean()
+
+    garden = fields.Boolean()
+
+    garden_erea = fields.Integer()
+
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
